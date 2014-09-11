a009a075e251616ec8534095b195feb5f02615ab
cpttripzz <zach.erskine@gmail.com>
2014-09-11 17:02:44 +0300

    php form working

--- app/scripts/controllers/dialog.js
+++ app/scripts/controllers/dialog.js
@@ -42,7 +42,7 @@ angular.module('bandaidApp',['ui.bootstrap','dialogs.main'])
 
         $scope.doLogin= function () {
           debugger;
-          var loginPromise = $http.post('http://api.bandaid.com/user/login', $scope.login);
+          var loginPromise = $http.post('http://api.bandaid.com/user/login', this.login);
 
         };
 
@@ -56,6 +56,59 @@ angular.module('bandaidApp',['ui.bootstrap','dialogs.main'])
             }
         };
     })
+  .config(['$sceDelegateProvider', function($sceDelegateProvider) {
+
+    $sceDelegateProvider.resourceUrlWhitelist(['self', 'http://api.bandaid.com/user/login']);
+
+  }])
+
+  .config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+    /**
+     * The workhorse; converts an object to x-www-form-urlencoded serialization.
+     * @param {Object} obj
+     * @return {String}
+     */
+    var param = function(obj) {
+      var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+
+      for(name in obj) {
+        value = obj[name];
+
+        if(value instanceof Array) {
+          for(i=0; i<value.length; ++i) {
+            subValue = value[i];
+            fullSubName = name + '[' + i + ']';
+            innerObj = {};
+            innerObj[fullSubName] = subValue;
+            query += param(innerObj) + '&';
+          }
+        }
+        else if(value instanceof Object) {
+          for(subName in value) {
+            subValue = value[subName];
+            fullSubName = name + '[' + subName + ']';
+            innerObj = {};
+            innerObj[fullSubName] = subValue;
+            query += param(innerObj) + '&';
+          }
+        }
+        else if(value !== undefined && value !== null)
+          query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+      }
+
+      return query.length ? query.substr(0, query.length - 1) : query;
+    };
+
+    // Override $http service's default transformRequest
+    $httpProvider.defaults.transformRequest = [function(data) {
+      return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+    }];
+  }])
 
   .config(['dialogsProvider',function(dialogsProvider){
     dialogsProvider.useBackdrop('static');
