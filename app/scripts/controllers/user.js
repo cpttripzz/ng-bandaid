2d6d1f918331bf5c7f7c6b1c8a59ed23f5cfe673
cpttripzz <zach.erskine@gmail.com>
2014-09-19 18:15:26 +0300

    do some auth stuff

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -7,53 +7,71 @@
  * # dialogCtrl
  * Controller of the bandaidApp
  */
-app.controller('userDialogCtrl', function ($scope, $rootScope, $modalInstance) {
+app.controller('userDialogCtrl', function ($scope, $rootScope, $modalInstance, authService) {
 
-  //-- Methods --//
+    //-- Methods --//
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('Canceled');
-  }; // end cancel
+    $scope.cancel = function () {
+        $modalInstance.dismiss('Canceled');
+    }; // end cancel
 
-  $scope.save = function () {
+    $scope.save = function () {
 
-  }; // end save
+    }; // end save
 
-  $scope.hitEnter = function (evt) {
-    if (angular.equals(evt.keyCode, 13) && !(angular.equals($scope.user.name, null) || angular.equals($scope.user.name, ''))) {
-      $scope.save();
+    $scope.hitEnter = function (evt) {
+        if (angular.equals(evt.keyCode, 13) && !(angular.equals($scope.user.name, null) || angular.equals($scope.user.name, ''))) {
+            $scope.save();
+        }
+    };
+    $scope.loginForm = {};
+    $scope.submitLogin = function (loginForm) {
+        authService.login($scope.loginForm.email, $scope.loginForm.password).then(function () {
+            $modalInstance.close();
+        },
+           function (error) {
+               $scope.loginForm.errors = error.data.reasons;
+            }
+        );
     }
-  };
-
 
 
 })
 
-  .config(['$sceDelegateProvider', 'commonServiceFactoryProvider', function ($sceDelegateProvider, commonServiceFactory) {
-    var apiConfig = commonServiceFactory.$get().getApiConfig();
-    $sceDelegateProvider.resourceUrlWhitelist(['self', apiConfig.baseUri + '/user/*']);
-  }])
+    .config(['$sceDelegateProvider', 'commonServiceFactoryProvider', function ($sceDelegateProvider, commonServiceFactory) {
+        var apiConfig = commonServiceFactory.$get().getApiConfig();
+        $sceDelegateProvider.resourceUrlWhitelist(['self', apiConfig.baseUri + '/user/*']);
+    }])
 
-  .config(['$httpProvider', 'commonServiceFactoryProvider', function ($httpProvider, commonServiceFactory) {
-    $httpProvider.defaults.useXDomain = true;
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+    .config(['$httpProvider', 'commonServiceFactoryProvider', function ($httpProvider, commonServiceFactory) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+        // get callable from provider
+        var getFormAsParams = commonServiceFactory.$get().getFormAsParams;
+        // Override $http service's default transformRequest
+        $httpProvider.defaults.transformRequest = [function (data) {
+            return angular.isObject(data) && String(data) !== '[object File]' ? getFormAsParams(data) : data;
+        }];
+    }])
 
-    // get callable from provider
-    var getFormAsParams = commonServiceFactory.$get().getFormAsParams();
-    // Override $http service's default transformRequest
-    $httpProvider.defaults.transformRequest = [function (data) {
-      return angular.isObject(data) && String(data) !== '[object File]' ? getFormAsParams(data) : data;
-    }];
-  }])
+    .config(['dialogsProvider', function (dialogsProvider) {
+        dialogsProvider.useBackdrop('static');
+        dialogsProvider.useEscClose(true);
+        dialogsProvider.useCopy(true);
+        dialogsProvider.setSize('sm');
+    }])
 
-  .config(['dialogsProvider', function (dialogsProvider) {
-    dialogsProvider.useBackdrop('static');
-    dialogsProvider.useEscClose(true);
-    dialogsProvider.useCopy(true);
-    dialogsProvider.setSize('sm');
-  }])
+    .run(['$templateCache', function ($templateCache) {
+        $templateCache.put('dialogs/login.html', '' +
+        '<div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-hidden="true" ng-click="cancel()">×</button> <h4 class="modal-title">Login</h4> </div> ' +
+        '<div class="modal-body"> <form novalidate ng-submit="submitLogin(loginForm)"> ' +
+        '<div class="form-group"  ng-class="{ \'has-error\' : loginForm.email.$invalid }"><label>Email</label>' +
+        '   <input type="email" name="email" ng-model="loginForm.email" required="required" class="form-control"/> ' +
+        '   <p ng-show="loginForm.email.$invalid" class="help-block">Enter a valid email.</p>' +
+        '   <span class="help-block" ng-show="loginForm.email.$error.required">Required</span>' +
+        '<div class="form-group"> <label>password</label> <input type="password" name="password" ng-model="loginForm.password" required="required" class="form-control"/></div>' +
 
-  .run(['$templateCache', function ($templateCache) {
-    $templateCache.put('dialogs/login.html', '<div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-hidden="true" ng-click="cancel()">×</button> <h4 class="modal-title">Login</h4> </div> <div class="modal-body"> <form ng-submit="submitLogin(loginForm)" role="form" ng-init="loginForm = {}"> <div class="form-group"> <label>email</label> <input type="email" name="email" ng-model="loginForm.email" required="required" class="form-control"/> </div> <div class="form-group"> <label>password</label> <input type="password" name="password" ng-model="loginForm.password" required="required" class="form-control"/> </div> <button type="submit" class="btn btn-primary btn-lg">Sign in</button> </form> <div class="error"></div> </div> ');
-  }]);
+        '<button  ng-disabled="loginForm.$invalid" type="submit" class="btn btn-primary btn-lg">Sign in</button> ' +
+        '<div ng-model="loginForm.errors">{{loginForm.errors}}</div></form>  </div> ');
+    }]);
--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -8,29 +8,33 @@
  * Service in the bandaidApp.
  */
 
-app.service('authService', ['$http', '$q', 'commonService', function ($http,$q) {
-    debugger;
-    //var apiConfig = commonService.APIConfig();
-    //var urlBase = apiConfig.baseUri + '/' + apiConfig.loginPath;
-    var userInfo ={};
-    this.login = function (userName, password) {
-      var deferred = $q.defer();
+app.service('authService', ['$http', '$q', 'commonServiceFactory', '$sessionStorage', '$rootScope',
+    function ($http, $q, commonServiceFactory, $sessionStorage, $rootScope) {
+        var apiConfig = commonServiceFactory.getApiConfig();
+        var urlBase = apiConfig.baseUri + apiConfig.loginPath;
+        var user = {};
+        this.login = function (email, password) {
+            var deferred = $q.defer();
+            try {
+                $http.post(urlBase, {
+                    email: email,
+                    password: password
+                }).then(function (result) {
+                    user = {
+                        token: result.data.user.token,
+                        email: result.data.user.email
+                    };
+                    $sessionStorage.user = user;
+                    $rootScope.$broadcast('userLoggedIn', user);
+                    deferred.resolve(user);
+                }, function (error) {
+                    deferred.reject(error);
+                });
+            } catch (e){
+                deferred.reject(e);
+            }
+            return deferred.promise;
+        }
 
-      $http.post(apiConfig.loginPath, {
-        userName: userName,
-        password: password
-      }).then(function (result) {
-        userInfo = {
-          accessToken: result.token,
-          userName: result.email
-        };
-        //$sessionStorage.deferred.resolve(userInfo);
-      }, function (error) {
-        deferred.reject(error);
-      });
-
-      return deferred.promise;
     }
-
-  }
 ]);
