fe35d772c90dd3349f9df4eb86f0bc5507696fd0
cpttripzz <zach.erskine@gmail.com>
2014-10-09 15:53:21 +0300

    add more code to home again

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -10,11 +10,12 @@
 app.controller('HomeCtrl', function ($scope,ApiService,commonServiceFactory) {
     var apiConfig = commonServiceFactory.getApiConfig();
     var imgPath = apiConfig.baseUri + apiConfig.imgPath;
+    $scope.bandPath = apiConfig.baseUri + '/band';
     $scope.userImgPath = imgPath + '/users';
     $scope.genreImgPath = imgPath + '/genres';
     $scope.flagImgPath = imgPath + '/flags';
     ApiService.getHomeItems().then(function (items) {
-            $scope.items = items;
+            $scope.items = items.data;
         },
         function (error) {
             console.log(error);
